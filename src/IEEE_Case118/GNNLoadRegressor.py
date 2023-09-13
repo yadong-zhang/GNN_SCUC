e67b4a102c4283d5ce48adce660ae3778229be37
Yadong Zhang <yadong.zhang@vanderbilt.edu>
2023-09-13 15:08:41 -0500

    Update solutions

--- src/IEEE_Case118/GNNLoadRegressor.py
+++ src/IEEE_Case118/GNNLoadRegressor.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 from torch.functional import F
-from torch_geometric.nn import GCNConv, GINConv, MLP
+from torch_geometric.nn import GCNConv, GATConv, GINConv, MLP
 
 class GNNLoadRegressor(nn.Module):
     def __init__(self, input_dim=32, hidden_dim=24, output_dim=12):
@@ -24,14 +24,33 @@ class GNNLoadRegressor(nn.Module):
             nn.ReLU(),
             nn.Linear(self.hidden_dim, self.hidden_dim),
             nn.ReLU(),
+            nn.Linear(self.hidden_dim, self.hidden_dim),
+            nn.ReLU(),
+            nn.Linear(self.hidden_dim, self.hidden_dim),
+            nn.ReLU(),
             nn.Linear(self.hidden_dim, self.output_dim),
             nn.ReLU(),
         )
 
+        # PyG MLP layers
+        self.mlp = MLP(in_channels=self.input_dim, hidden_channels=self.hidden_dim, out_channels=output_dim, num_layers=3, dropout=0.3)
+
+        # PyG GAT layers
+        self.gat = nn.ModuleList([
+            GATConv(self.input_dim, self.hidden_dim, heads=10),
+            GATConv(self.hidden_dim*10, self.hidden_dim, heads=5),
+            GATConv(self.hidden_dim*5, self.hidden_dim, heads=2),
+            GATConv(self.hidden_dim*2, self.hidden_dim),
+            GATConv(self.hidden_dim, self.output_dim)
+        ])
+
     
-    def forward(self, x, edge_index):
+    def forward(self, x, edge_index, edge_attr):
 
         # ANN layers
-        x = self.ann(x)
+        # x = self.ann(x)
+        for layer in self.gat:
+            x = layer(x, edge_index, edge_attr)
+            x = F.relu(x)
 
         return x
