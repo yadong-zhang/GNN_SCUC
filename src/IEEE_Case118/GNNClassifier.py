bfdf0a77e091a88ab76c1883deff805cff474307
Yadong Zhang <yadong.zhang@vanderbilt.edu>
2023-09-16 15:17:57 -0500

    Update solutions

--- src/IEEE_Case118/GNNClassifier.py
+++ src/IEEE_Case118/GNNClassifier.py
@@ -9,11 +9,62 @@ import torch
 import torch.nn as nn
 from torch.functional import F
 from torch_geometric.nn import MLP
-from torch_geometric.nn import GCNConv, GINConv
+from torch_geometric.nn import GCNConv, SAGEConv, Sequential
 
-class GNNClassifier(nn.Module):
-    def __init__(self, input_dim=32, hidden_dim=32, output_dim=32):
-        super(GNNClassifier, self).__init__()
+class UCSAGE(nn.Module):
+    def __init__(self, input_dim=32, hidden_dim=24, output_dim=12):
+        super(UCSAGE, self).__init__()
+
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+
+        # SAGEConv
+        self.gnn = Sequential('x, edge_index', [
+            (SAGEConv(self.input_dim, self.hidden_dim), 'x, edge_index -> x'),
+            nn.ReLU(),
+            (SAGEConv(self.hidden_dim, self.hidden_dim), 'x, edge_index -> x'),
+            nn.ReLU(),
+            (SAGEConv(self.hidden_dim, self.output_dim), 'x, edge_index -> x'),
+            nn.Sigmoid()
+        ])
+    
+    def forward(self, x, edge_index):
+
+        # SAGEConv
+        x = self.gnn(x, edge_index)
+
+        return x
+    
+class UCGCN(nn.Module):
+    def __init__(self, input_dim=32, hidden_dim=24, output_dim=12):
+        super(UCGCN, self).__init__()
+
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+
+        # GCNConv
+        self.gnn = Sequential('x, edge_index', [
+            (GCNConv(self.input_dim, self.hidden_dim), 'x, edge_index -> x'),
+            nn.ReLU(),
+            (GCNConv(self.hidden_dim, self.hidden_dim), 'x, edge_index -> x'),
+            nn.ReLU(),
+            (GCNConv(self.hidden_dim, self.output_dim), 'x, edge_index -> x'),
+            nn.Sigmoid()
+        ])
+    
+    def forward(self, x, edge_index):
+
+        # GCNConv
+        x = self.gnn(x, edge_index)
+
+        return x
+    
+
+class UCANN(nn.Module):
+    def __init__(self, input_dim=32, hidden_dim=24, output_dim=12):
+        super(UCANN, self).__init__()
 
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
@@ -28,8 +79,8 @@ class GNNClassifier(nn.Module):
             nn.Linear(self.hidden_dim, self.output_dim),
             nn.Sigmoid()
         )
-    
-    def forward(self, x):
+
+    def forward(self, x, edge_index):
 
         # ANN layers
         x = self.ann(x)
