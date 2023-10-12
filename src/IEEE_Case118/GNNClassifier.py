ef410a2d714777bc2b51837727c56036b087184e
Yadong Zhang <yadong.zhang@vanderbilt.edu>
2023-10-12 12:39:24 -0500

    Update solutions

--- src/IEEE_Case118/GNNClassifier.py
+++ src/IEEE_Case118/GNNClassifier.py
@@ -11,6 +11,31 @@ from torch.functional import F
 from torch_geometric.nn import MLP
 from torch_geometric.nn import GCNConv, SAGEConv, Sequential
 
+class UCANN(nn.Module):
+    def __init__(self, input_dim=32, hidden_dim=24, output_dim=12):
+        super(UCANN, self).__init__()
+
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+
+        # ANN layers
+        self.ann = nn.Sequential(
+            nn.Linear(self.input_dim, self.hidden_dim),
+            nn.ReLU(),
+            nn.Linear(self.hidden_dim, self.hidden_dim),
+            nn.ReLU(),
+            nn.Linear(self.hidden_dim, self.output_dim),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x, edge_index):
+
+        # ANN layers
+        x = self.ann(x)
+
+        return x
+
 class UCSAGE(nn.Module):
     def __init__(self, input_dim=32, hidden_dim=24, output_dim=12):
         super(UCSAGE, self).__init__()
@@ -58,31 +83,5 @@ class UCGCN(nn.Module):
 
         # GCNConv
         x = self.gnn(x, edge_index)
-
-        return x
-    
-
-class UCANN(nn.Module):
-    def __init__(self, input_dim=32, hidden_dim=24, output_dim=12):
-        super(UCANN, self).__init__()
-
-        self.input_dim = input_dim
-        self.hidden_dim = hidden_dim
-        self.output_dim = output_dim
-
-        # ANN layers
-        self.ann = nn.Sequential(
-            nn.Linear(self.input_dim, self.hidden_dim),
-            nn.Tanh(),
-            nn.Linear(self.hidden_dim, self.hidden_dim),
-            nn.Tanh(),
-            nn.Linear(self.hidden_dim, self.output_dim),
-            nn.Sigmoid()
-        )
-
-    def forward(self, x, edge_index):
-
-        # ANN layers
-        x = self.ann(x)
 
         return x
